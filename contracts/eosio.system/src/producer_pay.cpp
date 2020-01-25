56ebc6e0d56f11dc509622f4e4813f0be6e77308
craigbranscom <craigbranscom@gmail.com>
2020-01-25 16:17:03 -0500

    redirect TEDP funds to Telos Works

--- contracts/eosio.system/src/producer_pay.cpp
+++ contracts/eosio.system/src/producer_pay.cpp
@@ -156,7 +156,7 @@ namespace eosiosystem {
             }
             
             if(to_workers > 0) {
-                transfer_act.send(get_self(), decide_account, asset(to_workers, core_symbol()), "Transfer worker proposal share to telos.decide account");
+                transfer_act.send(get_self(), works_account, asset(to_workers, core_symbol()), "Transfer worker proposal share to works.decide account");
             }
             
             if(to_producers > 0) {
