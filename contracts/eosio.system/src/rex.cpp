ded17aad4925884cbbb06b82fa4296e49b36c59f
Douglas Horn <37677685+DouglasHorn@users.noreply.github.com>
2020-10-06 16:20:20 -0700

    Update rex.cpp to remove REX voting requirement
    Remove the requirement to vote 21 BPs or select a proxy in order to stake TLOS to REX

--- contracts/eosio.system/src/rex.cpp
+++ contracts/eosio.system/src/rex.cpp
@@ -42,7 +42,7 @@ namespace eosiosystem {
 
       check( amount.symbol == core_symbol(), "asset must be core token" );
       check( 0 < amount.amount, "must use positive amount" );
-      check_voting_requirement( from );
+      // check_voting_requirement( from );            Remove requirement to vote 21 BPs or select a proxy to stake to REX
       transfer_from_fund( from, amount );
       const asset rex_received    = add_to_rex_pool( amount );
       const asset delta_rex_stake = add_to_rex_balance( from, amount, rex_received );
@@ -60,7 +60,7 @@ namespace eosiosystem {
       check( from_net.symbol == core_symbol() && from_cpu.symbol == core_symbol(), "asset must be core token" );
       check( (0 <= from_net.amount) && (0 <= from_cpu.amount) && (0 < from_net.amount || 0 < from_cpu.amount),
              "must unstake a positive amount to buy rex" );
-      check_voting_requirement( owner );
+      // check_voting_requirement( owner );            Remove requirement to vote 21 BPs or select a proxy to stake to REX 
 
       {
          del_bandwidth_table dbw_table( get_self(), owner.value );
@@ -420,7 +420,7 @@ namespace eosiosystem {
    void system_contract::check_voting_requirement( const name& owner, const char* error_msg )const
    {
       auto vitr = _voters.find( owner.value );
-      check( vitr != _voters.end() && ( vitr->proxy || 21 <= vitr->producers.size() ), error_msg );
+      check( vitr != _voters.end() && ( vitr->proxy || 0 <= vitr->producers.size() ), error_msg );       // Required voters set to 0 
    }
 
    /**
